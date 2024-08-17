2663f60d5d396e2dfb77ebbc7b0e07d79e356088
Ricardo Q. <ricardoqg41@icloud.com>
2024-08-16 21:01:16 -0500

    add layouts

---
+++ src/layouts/home/index.js
@@ -0,0 +1,15 @@
+import styles from "./home.module.css";
+import RecentTrack from "../../components/recentTrack";
+
+export default function Home() {
+    return (
+        <>
+            <div className={styles.container}>
+                <h1>Home</h1>
+            </div>
+            <div className={styles.recentTrackWrapper}>
+                <RecentTrack />
+            </div>
+        </>
+    );
+}
