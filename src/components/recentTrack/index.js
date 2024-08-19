54ddf2905c37793721d2c56bc4cde914d424580d
Ricardo Q. <ricardoqg41@icloud.com>
2024-08-19 01:27:20 -0500

    change of style

--- src/components/recentTrack/index.js
+++ src/components/recentTrack/index.js
@@ -2,7 +2,7 @@ import styles from "./recentTrack.module.css";
 
 export default function recentTrack() {
     return (
-        <div className={styles.container}><h1>Recent Tracks</h1>
+        <div className={styles.container}>
             <div className="cc_recenttracks_list" data-username="demo">Cargando ...</div>
             {/* Aquí puedes agregar la lista de canciones recientes */}
         </div>
--- src/layouts/home/index.js
+++ src/layouts/home/index.js
@@ -1,17 +1,18 @@
 import styles from "./home.module.css";
 import RecentTrack from "../../components/recentTrack";
+import Footer from "../footer";
 
 export default function Home() {
     return (
         <>
             <div className={styles.container}>
-                <h1 className={styles.modal}>Home</h1>
-
-            <div className={styles.recentTrackWrapper}>
-                <RecentTrack />
+                <h1 className={styles.modal}>Radio</h1>
+                <div className={styles.recentTrackWrapper}>
+                    <RecentTrack />
+                </div>
             </div>
 
-            </div>
+            <Footer />
         </>
     );
 }
