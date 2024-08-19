b823a1d176547a73385d92aa096d2d472e27aa15
Ricardo Q. <ricardoqg41@icloud.com>
2024-08-19 00:57:25 -0500

    add footer

---
+++ src/layouts/footer/index.js
@@ -0,0 +1,11 @@
+import styles from './footer.module.css'
+
+export default function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <div className="container">
+                <p>© 2024 - All Rights Reserved</p>
+            </div>
+        </footer>
+    );
+}
