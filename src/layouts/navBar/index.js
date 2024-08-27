5613f7c7aab89c4a98f18f5c4eef25428ebd2cd5
Ricardo Q. <ricardoqg41@icloud.com>
2024-08-27 18:12:20 -0500

    add Boostrap icons, button modal and import component requestSong

--- src/layouts/navBar/index.js
+++ src/layouts/navBar/index.js
@@ -1,49 +1,63 @@
+
+
+import SongRequest from "../../components/songRequest";
+
+
 export default function NavBar() {
+
+
+
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-black" data-bs-theme="dark">
-            <div className="container">
-                <a className="navbar-brand" href="/">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
-                         className="bi bi-broadcast text-danger" viewBox="0 0 16 16">
-                        <path
-                            d="M3.05 3.05a7 7 0 0 0 0 9.9.5.5 0 0 1-.707.707 8 8 0 0 1 0-11.314.5.5 0 0 1 .707.707m2.122 2.122a4 4 0 0 0 0 5.656.5.5 0 1 1-.708.708 5 5 0 0 1 0-7.072.5.5 0 0 1 .708.708m5.656-.708a.5.5 0 0 1 .708 0 5 5 0 0 1 0 7.072.5.5 0 1 1-.708-.708 4 4 0 0 0 0-5.656.5.5 0 0 1 0-.708m2.122-2.12a.5.5 0 0 1 .707 0 8 8 0 0 1 0 11.313.5.5 0 0 1-.707-.707 7 7 0 0 0 0-9.9.5.5 0 0 1 0-.707zM10 8a2 2 0 1 1-4 0 2 2 0 0 1 4 0"/>
-                    </svg>
-                </a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                        aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Inicio</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Contacto</a>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                               aria-expanded="false">
-                                Dropdown
-                            </a>
-                            <ul className="dropdown-menu">
-                                <li><a className="dropdown-item" href="#">Action</a></li>
-                                <li><a className="dropdown-item" href="#">Another action</a></li>
-                                <li>
-                                    <hr className="dropdown-divider"/>
-                                </li>
-                                <li><a className="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
-                        <div className="vr bg-light"></div>
-                        <li className="nav-item">
-                            <a className="nav-link disabled" aria-disabled="true">En Construcion</a>
-                        </li>
-                    </ul>
-                    <button type="button" className="btn btn-outline-danger" disabled>Pide tu cancion</button>
+        <>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-black">
+                <div className="container">
+                    <a className="navbar-brand" href="/">
+                        <i className="bi bi-broadcast" style={{fontSize: '1.875rem', color: 'red'}}></i>
+                    </a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Inicio</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Contacto</a>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                   aria-expanded="false">
+                                    Dropdown
+                                </a>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="#">Action</a></li>
+                                    <li><a className="dropdown-item" href="#">Another action</a></li>
+                                    <li>
+                                        <hr className="dropdown-divider"/>
+                                    </li>
+                                    <li><a className="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                            </li>
+                            <div className="vr bg-light"></div>
+                            <li className="nav-item">
+                                <a className="nav-link disabled" aria-disabled="true">En Construcion</a>
+                            </li>
+                        </ul>
+                        <button type="button"
+                                className="btn btn-outline-danger"
+                                data-bs-toggle="modal"
+                                data-bs-target="#staticBackdrop"
+                                disabled
+                        >Pide tu cancion
+                        </button>
+                    </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+
+            <SongRequest />
+        </>
     );
 }
