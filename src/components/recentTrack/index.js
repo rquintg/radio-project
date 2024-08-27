ca72964be6dd17990b921188d2ecc8a7cd5dadeb
Ricardo Q. <ricardoqg41@icloud.com>
2024-08-27 18:16:24 -0500

    add recentTracks V1

--- src/components/recentTrack/index.js
+++ src/components/recentTrack/index.js
@@ -3,8 +3,11 @@ import styles from "./recentTrack.module.css";
 export default function recentTrack() {
     return (
         <div className={styles.container}>
-            <div className="cc_recenttracks_list" data-username="demo">Cargando ...</div>
-            {/* Aquí puedes agregar la lista de canciones recientes */}
+            <div className="cc_recenttracks_list" data-username="demo">
+                <div className="spinner-border text-danger" role="status">
+                    <span className="visually-hidden"></span>
+                </div>
+            </div>
         </div>
     );
 }
