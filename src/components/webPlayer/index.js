44a3b0f284486ee964e0a35df873c691a7fcc5b3
Ricardo Q. <ricardoqg41@icloud.com>
2024-08-30 19:44:42 -0500

    add web player component V1

---
+++ src/components/webPlayer/index.js
@@ -0,0 +1,23 @@
+import PunkPlayer from 'react-player'
+import styles from "./webPlayer.module.css";
+
+export default function WebPlayer() {
+    return (
+        <div className="container">
+           {/*<PunkPlayer
+               url='https://stream10.usastreams.com/9316/stream'
+               playing={true}
+               controls={true}
+           />*/}
+
+            <div className={styles.cont}>
+                <audio controls>
+                    <source src="https://stream10.usastreams.com/9316/stream" type="audio/mpeg"/>
+                    Tu navegador no soporta este elemento
+                </audio>
+            </div>
+
+
+        </div>
+    )
+}
