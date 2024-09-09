feda386bda699d7010349c9146c9c3027f0b4a91
Ricardo Q. <ricardoqg41@icloud.com>
2024-09-08 21:21:47 -0500

    add title

--- src/components/recentTrack/index.js
+++ src/components/recentTrack/index.js
@@ -3,6 +3,7 @@ import styles from "./recentTrack.module.css";
 export default function recentTrack() {
     return (
         <div className={styles.container}>
+            <h6 className={styles.title}>Esta Sonando</h6>
             <div className="cc_recenttracks_list" data-username="demo">
                 <div className="spinner-border text-danger" role="status">
                     <span className="visually-hidden"></span>
