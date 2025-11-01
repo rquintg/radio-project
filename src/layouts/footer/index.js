5874612947655fd7d0fe5bc214a73338983e9724
Ricardo Q. <ricardoqg41@icloud.com>
2025-11-01 14:07:25 -0500

    update month and year with Date Objet

--- src/layouts/footer/index.js
+++ src/layouts/footer/index.js
@@ -1,10 +1,14 @@
 import styles from './footer.module.css'
 
 export default function Footer() {
+
+    const ahora = new Date();
+    const mesYanio = Intl.DateTimeFormat('es-ES', { month: 'short', year: 'numeric' }).format(ahora);
+
     return (
         <footer className={styles.footer}>
             <div className="container">
-                <p>© 2024 - All Rights Reserved</p>
+                <p>© {mesYanio} - All Rights Reserved</p>
             </div>
         </footer>
     );
