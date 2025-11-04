047e9b36945259743ba54ae1e4eedee49688cc9f
Ricardo Q. <ricardoqg41@icloud.com>
2025-11-04 04:14:58 -0500

    add logo in home

--- src/layouts/home/index.js
+++ src/layouts/home/index.js
@@ -1,4 +1,5 @@
 import styles from "./home.module.css";
+import PunkM from "../../img/Logo-Punk-Medallo-2024 Blanco.png";
 import RecentTrack from "../../components/recentTrack";
 import PunkPlayer from "../../components/webPlayer";
 import StaffComponent from "../../components/staffComponent";
@@ -7,7 +8,9 @@ export default function Home() {
     return (
         <>
             <div className={styles.container}>
-                <h1 className={styles.modal}>Radio</h1>
+                <div className={styles.modal}>
+                    <img src={PunkM} width="380px" alt="punk medallo"/>
+                </div>
                 <div className={styles.recentTrackWrapper}>
                     <RecentTrack />
                 </div>
