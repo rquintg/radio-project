70d5621f51b97126886f76a0d2828cb978eea82a
Ricardo Q. <ricardoqg41@icloud.com>
2025-11-09 16:02:15 -0500

    add WebPlayer Component

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,20 +5,23 @@ import Home from "../layouts/home";
 import About from "../layouts/about";
 import Contact from "../layouts/contact";
 import Blog from "../layouts/blog";
+import WebPlayer from "../components/webPlayer";
 
 export default function MainRouter() {
     return (
         <>
             <NavBar />
-            <Routes>
-                <Route path="/" element={<Home/>} />
-                <Route path="/blog" element={<Blog/>} />
-                <Route path="/contact" element={<Contact/>} />
-                <Route path="/about" element={<About/>} />
-            </Routes>
-
+            <div style={{ paddingBottom: '90px' }}>
+                <Routes>
+                    <Route path="/" element={<Home/>} />
+                    <Route path="/blog" element={<Blog/>} />
+                    <Route path="/contact" element={<Contact/>} />
+                    <Route path="/about" element={<About/>} />
+                </Routes>
+            </div>
+            {/* Reproductor persistente (no se desmonta al cambiar de ruta) */}
+            <WebPlayer />
         </>
 
     );
 }
-
