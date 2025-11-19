5c402f9cab553a0c63c95a0743f6a6ba4d914fdb
Ricardo Q. <ricardoqg41@icloud.com>
2025-11-19 04:05:04 -0500

    Add loading spinner to recent track history
    Introduces a loading spinner that displays while the recent track history iframe is loading. Also improves CSS alignment and styling for better visual presentation.

--- src/components/recentTrack/index.js
+++ src/components/recentTrack/index.js
@@ -1,10 +1,19 @@
 import styles from "./recentTrack.module.css";
 import SpinnerLoader from "../util/spinnerLoader";
+import {useState } from "react";
 
 export default function RecentTrack() {
+
+    const [isLoading, setIsLoading]= useState(true);
+
+    const handleLoad = () => {
+        setIsLoading(false);
+    };
+
   return (
     <div className={styles.container}>
       <h6 className={styles.title}>Historial de Canciones</h6>
+      {isLoading && <SpinnerLoader />}
       <div className={styles.iframeWrapper}>
         <iframe
           src="https://lsdradiohostserver.com/public/punk_hc/history?theme=dark"
@@ -12,6 +21,7 @@ export default function RecentTrack() {
           allowtransparency="true"
           title="Historial de canciones"
           className={styles.historyFrame}
+          onLoad={handleLoad}
         />
       </div>
     </div>
