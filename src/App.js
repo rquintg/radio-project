aa5fb80f13c13050ad87c357c5edfb4a9c3c29c9
Ricardo Q. <ricardoqg41@icloud.com>
2025-12-10 18:35:57 -0500

    implement React Floating Whatsapp

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import { SpeedInsights } from '@vercel/speed-insights/react';
-
-import './App.css';
+import { FloatingWhatsApp } from 'react-floating-whatsapp';
 import MainRouter from "./routes";
 import Footer from "./layouts/footer";
+
+import avatar from '../src/img/logo_punk_medallo.jpg';
+import './App.css';
+
 
 function App() {
   return (
@@ -10,7 +13,23 @@ function App() {
        <MainRouter/>
        <Footer />
        <SpeedInsights />
+    
+      <FloatingWhatsApp
+        phoneNumber="573014453392"
+        accountName="Punk Medallo"
+        statusMessage="Generalmente responde rapido"
+        chatMessage="Hola, ¿en qué puedo ayudarte?"
+        placeholder="Escribe un mensaje..."
+        avatar={avatar}
+        darkMode
+        allowEsc
+        allowClickAway
+        notification
+        notificationDelay={10}
+        buttonClassName='whatsappButton'
+      />
    </div>
+   
   );
 }
 
