acdd475dedd3d3f337d927feb603fe217553cf02
Ricardo Q. <ricardoqg41@icloud.com>
2026-04-21 20:50:06 -0500

    update info and image

--- src/components/staffComponent/index.js
+++ src/components/staffComponent/index.js
@@ -1,26 +1,41 @@
 import styles from "./staffComponent.module.css";
 import PunkM from "../../img/logo_punk_medallo.jpg";
-import PunkV from "../../img/punk_en_las_venas.png";
-import RadioR from "../../img/lsdradio.jpeg";
+import Toques from "../../img/toques.webp";
+import Blog from "../../img/blog.jpg";
 
 export  default function staffComponent (){
     return(
         <>
             <div className={`${styles.contenedor} ${styles.sombra}`}>
-                <h2 style={{fontWeight: 'bold'}}>Staff</h2>
+                <h2 style={{fontWeight: 'bold'}}>De interes</h2>
 
                 <div className={styles.servicios}>
 
                     <div className={styles.servicio}>
 
-                        <a href="https://www.facebook.com/punkenlasvenas1" target="_blank" rel="noopener noreferrer">
-                            <h3>Punk en las Venas</h3>
+                        <a href="https://www.instagram.com/punk.medallo" target="_blank" rel="noopener noreferrer">
+                            <h3>Cartelera de Eventos</h3>
+
                             <div className={styles.iconos}>
-                                <img src={PunkV} alt="punk_en_las_venas"/>
+                                <img src={Toques} alt="radio ruidosa"/>
                             </div>
                         </a>
 
-                        <p>Apoyando la escena punk nacional colombiana </p>
+
+
+                        <p>Siguenos,escucha, apoya y difunde.</p>
+                    </div>
+
+                    <div className={styles.servicio}>
+
+                        <a href="https://punk-medallo.blogspot.com/" target="_blank" rel="noopener noreferrer">
+                            <h3>Blog</h3>
+                            <div className={styles.iconos}>
+                                <img src={Blog} alt="punk_en_las_venas"/>
+                            </div>
+                        </a>
+
+                        <p>Nuevo material disponible para descargar y links actualizados.</p>
                     </div>
 
                     <div className={styles.servicio}>
@@ -37,15 +52,6 @@ export  default function staffComponent (){
                             Hacemos memoria de nuestra huella en este lodazal.</p>
                     </div>
 
-                    <div className={styles.servicio}>
-                        <h3>Auspiciado Por</h3>
-
-                        <div className={styles.iconos}>
-                            <img src={RadioR} alt="radio ruidosa"/>
-                        </div>
-
-                        <p>Escucha, apoya y difunde.</p>
-                    </div>
                 </div>
             </div>
         </>
