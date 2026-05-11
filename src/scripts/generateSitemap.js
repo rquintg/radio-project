4f491bf82fd676b31226906b7a8601cb51442941
Ricardo Q. <ricardoqg41@icloud.com>
2026-05-11 02:06:17 -0500

    add amigos in sitemap

--- src/scripts/generateSitemap.js
+++ src/scripts/generateSitemap.js
@@ -11,6 +11,7 @@ const routes = [
   { path: '/about', changefreq: 'monthly', priority: 0.7 },
   { path: '/fotos', changefreq: 'daily', priority: 0.8 },
   { path: '/eventos', changefreq: 'daily', priority: 0.8 },
+  { path: '/amigos', changefreq: 'monthly', priority: 0.7 },
 ];
 
 const generateSitemap = () => {
